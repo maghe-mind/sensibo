a26b84361031877f5f10d12363e458a4622385a3
Unknown <maghe@mind.cc>
2017-11-14 18:43:09 +0100

    Refactored Post method

--- main.cpp
+++ main.cpp
@@ -7,15 +7,11 @@
 
 using json = nlohmann::json;
 
-std::shared_ptr<httplib::Response> getStates(std::string podUid, std::string basic_string);
+std::shared_ptr<httplib::Response> GetStates(std::string podUid, std::string basic_string);
 
 void PrintResponse(std::shared_ptr<httplib::Response> states);//TODO: const??
 
-
-
-
-
-
+std::shared_ptr<httplib::Response> PostAcState(std::string basic_string, std::string basicString, std::basic_string<char> dump, std::string type);
 
 std::string host = "home.sensibo.com"; //TODO: static and final??
 int port = 443;//TODO: static and final??
@@ -27,26 +23,10 @@ std::string apiKey = "FdNABENzMCuRt7niVUgSm8oxVvXi85"; //TODO: static and final?
 
 int main() {
 
-    auto states = getStates(podUid,apiKey);
+   // auto responseStates = GetStates(podUid,apiKey);
 
-    PrintResponse(states);
+    //PrintResponse(responseStates);
 
-/*
-    auto res = cli.get("/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85");
-    if (res && res->status == 200) {
-        std::cout << res->body << std::endl;
-
-        // parse explicitly
-        auto j3 = json::parse(res->body);
-        std::cout << "JSON" << std::endl;
-        std::cout << j3 << std::endl;
-    } else {
-        std::cout << "error!!!" << std::endl;
-    }
-
-
-    // correct post request
-    //{"acState": { "on": false, "targetTemperature": 28, "temperatureUnit": "C", "mode": "auto", "swing": "rangeFull" }}
     json j2 = {
             {"acState", {
                                 {"on", false},
@@ -57,43 +37,15 @@ int main() {
                         }
             }
     };
+    std::string contentType = "application/json";
 
-
-    std::string str = j2.dump();
-    std::cout << str << std::endl;
-
-    std::replace(str.begin(), str.end(), ' ', 'c');
-    std::cout << str << std::endl;
-
-    std::string content = "{\"acState\": { \"on\": false, \"targetTemperature\": 28, \"temperatureUnit\": \"C\", \"mode\": \"auto\", \"swing\": \"rangeFull\" }}";
-    std::cout << content << std::endl;
-
-
-    res = cli.post("/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85", str, "application/json");
-    if (res && res->status == 200) {
-        std::cout << res->body << std::endl;
-
-        // parse explicitly
-        auto j3 = json::parse(res->body);
-        std::cout << "JSON" << std::endl;
-        std::cout << j3 << std::endl;
-    } else {
-        std::cout << "error!!!" << std::endl;
-        std::cout << res->body << std::endl;
-    }
-    // https://home.sensibo.com/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85
-
-
-*/
-
-
-
-
-
-
+    auto responsePost = PostAcState (podUid,apiKey,j2.dump(),contentType);
+    PrintResponse(responsePost);
 
     return 0;
 }
+
+
 
 void PrintResponse(std::shared_ptr<httplib::Response> states) {
     if (states) {
@@ -114,10 +66,16 @@ void PrintResponse(std::shared_ptr<httplib::Response> states) {
     }
 }
 
-std::shared_ptr<httplib::Response> getStates(std::string uid, std::string apiKey) {
+std::shared_ptr<httplib::Response> GetStates(std::string uid, std::string apiKey) {
 
     std::string path = "/api/v2/pods/" + uid + "/acStates?apiKey=" + apiKey;
     std::shared_ptr<httplib::Response> res = cli.get(path.c_str());
     return res;
 
+}
+
+std::shared_ptr<httplib::Response> PostAcState(std::string uid, std::string apiKey, std::basic_string<char> message, std::string contentType) {
+    std::string path = "/api/v2/pods/" + uid + "/acStates?apiKey=" + apiKey;
+    std::shared_ptr<httplib::Response> res = cli.post(path.c_str(), message, contentType.c_str());
+    return res;
 }
