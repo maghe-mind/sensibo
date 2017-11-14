d8046b19af4d8db6c58fe3447e7735db1081edb1
Unknown <maghe@mind.cc>
2017-11-14 17:36:39 +0100

    Post request demo
    Post request works fine. Code refactoring is mandatory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
+
 #include "httplib.h"
 #include "json.hpp"
 #include <iostream>
@@ -9,7 +10,17 @@ using json = nlohmann::json;
 
 int main() {
 
-    httplib::SSLClient cli("home.sensibo.com", 443);
+    std::string host = "home.sensibo.com";
+    int port = 443;
+
+    // std::string podUid = "kvDso2fP";
+   // std::string apiKey = "FdNABENzMCuRt7niVUgSm8oxVvXi85";
+
+
+    httplib::SSLClient cli(host.c_str(), port);
+
+
+
 
     auto res = cli.get("/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85");
     if (res && res->status == 200) {
@@ -19,14 +30,36 @@ int main() {
         auto j3 = json::parse(res->body);
         std::cout << "JSON" << std::endl;
         std::cout << j3 << std::endl;
-    }
-    else  {
+    } else {
         std::cout << "error!!!" << std::endl;
     }
 
 
+    // correct post request
+    //{"acState": { "on": false, "targetTemperature": 28, "temperatureUnit": "C", "mode": "auto", "swing": "rangeFull" }}
+    json j2 = {
+            {"acState",{
+                                {"on",false},
+                                {"targetTemperature",28},
+                                {"temperatureUnit","C"},
+                                {"mode","auto"},
+                                {"swing","rangeFull"}
+                        }
+            }
+    };
 
-    res = cli.post()
+
+    std::string str = j2.dump();
+    std::cout << str << std::endl;
+
+    std::replace(str.begin(), str.end(), ' ', 'c');
+    std::cout << str << std::endl;
+
+    std::string content = "{\"acState\": { \"on\": false, \"targetTemperature\": 28, \"temperatureUnit\": \"C\", \"mode\": \"auto\", \"swing\": \"rangeFull\" }}";
+    std::cout << content << std::endl;
+
+
+  res = cli.post("/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85", str, "application/json");
     if (res && res->status == 200) {
         std::cout << res->body << std::endl;
 
@@ -34,11 +67,11 @@ int main() {
         auto j3 = json::parse(res->body);
         std::cout << "JSON" << std::endl;
         std::cout << j3 << std::endl;
-    }
-    else  {
+    } else {
         std::cout << "error!!!" << std::endl;
+        std::cout << res->body << std::endl;
     }
-    https://home.sensibo.com/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85
+   // https://home.sensibo.com/api/v2/pods/kvDso2fP/acStates?apiKey=FdNABENzMCuRt7niVUgSm8oxVvXi85
 
 
 
