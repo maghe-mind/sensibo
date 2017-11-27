6d6b9dcf7ca8d03a6a0efa1a74425a32fb6fad13
Unknown <maghe@mind.cc>
2017-11-27 11:32:08 +0100

    Created a define.h with config vars

---
+++ define.h
@@ -0,0 +1,12 @@
+//
+// Created by maghe on 24/11/17.
+//
+
+#ifndef SENSIBO_DEFINE_H_H
+#define SENSIBO_DEFINE_H_H
+
+#define SENSIBO_HOST "\home.sensibo.com\"
+#define SENSIBO_PORT 443
+#define SENSIBO_APIKEY "FdNABENzMCuRt7niVUgSm8oxVvXi85"
+
+#endif //SENSIBO_DEFINE_H_H
--- define.h.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by maghe on 24/11/17.
-//
-
-#ifndef SENSIBO_DEFINE_H_H
-#define SENSIBO_DEFINE_H_H
-
-#endif //SENSIBO_DEFINE_H_H
